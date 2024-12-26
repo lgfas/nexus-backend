bb5fd2bb7452b8115ee23d117575a945f7d61aea
lgfas <luis.guilherme2811@gmail.com>
2024-12-26 12:07:05 -0300

    feat: adicionando rota de tributos

--- apps/faturas/urls.py
+++ apps/faturas/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ContaEnergiaViewSet, ItemFaturaViewSet, UploadFaturaAPIView
+from .views import ContaEnergiaViewSet, ItemFaturaViewSet, UploadFaturaAPIView, TributoViewSet
 
 router = DefaultRouter()
 router.register(r'contas-energia', ContaEnergiaViewSet, basename='contaenergia')
 router.register(r'itens-fatura', ItemFaturaViewSet, basename='itemfatura')
+router.register(r'tributos', TributoViewSet, basename='tributo')
 
 urlpatterns = router.urls + [
     path('upload-fatura/', UploadFaturaAPIView.as_view(), name='upload-fatura'),
