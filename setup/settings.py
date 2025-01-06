4f1751c385898180e5d4b0178ce23e15993a0fd4
lgfas <luis.guilherme2811@gmail.com>
2025-01-06 16:45:06 -0300

    feat: implementação do swagger

--- setup/settings.py
+++ setup/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,7 @@ INSTALLED_APPS = [
     'apps.parceiros',
     'apps.historicos',
     'apps.tarifas',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'JSON_EDITOR': True,
+    'DEFAULT_MODEL_RENDERING': 'example',
+}
--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+# Configuração do Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Documentação da API",
+        default_version='v1',
+        description="Documentação do backend do projeto Nexus",
+        terms_of_service="https://www.seusite.com/termos/",
+        contact=openapi.Contact(email="suporte@seusite.com"),
+        license=openapi.License(name="Licença MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/clientes/', include('apps.clientes.urls')),
-    path('api/faturas/', include('apps.faturas.urls')),  # Faturas com prefixo exclusivo
+    path('api/faturas/', include('apps.faturas.urls')),
     path('api/parceiros/', include('apps.parceiros.urls')),
-    path('api/historicos/', include('apps.historicos.urls')),  # Historicos com prefixo exclusivo
+    path('api/historicos/', include('apps.historicos.urls')),
     path('api/tarifas/', include('apps.tarifas.urls')),
+
+    # Documentação com Swagger e ReDoc
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
